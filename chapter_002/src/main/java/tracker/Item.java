b3fe3c2089a80a5f9d1a4e2d7bfc3b3141a01007
Dmitrii Churbanov <dmitrii.churbanov@netcracker.com>
2020-06-22 20:37:38 +0400

    2. Перегрузка конструктора. [#228651]

---
+++ chapter_002/src/main/java/tracker/Item.java
@@ -0,0 +1,19 @@
+package tracker;
+
+public class Item {
+
+    private String name;
+    private String id;
+
+    public static void main(String[] args) {
+    }
+
+    public Item() {
+    }
+
+    public Item(String id) {
+    }
+
+    public Item(String name, String id) {
+    }
+}
