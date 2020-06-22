34200a5b83934222330e2bcc37d4d941772e865b
Dmitrii Churbanov <dmitrii.churbanov@netcracker.com>
2020-06-22 20:04:05 +0400

    4. Расстояние между точками в трехмерном пространстве. [#228637]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -7,14 +7,25 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point point) {
         return sqrt(pow(this.x - point.x, 2) + pow(this.y - point.y, 2));
+    }
+
+    public double distance3d(Point point) {
+        return sqrt(pow(this.x - point.x, 2) + pow(this.y - point.y, 2) + pow(this.z - point.z, 2));
     }
 
     public static void main(String[] args) {
@@ -24,3 +35,14 @@ public class Point {
         System.out.println(dist);
     }
 }
+//Задание.
+//
+//1. Перегрузите конструктор Point.
+//2. Реализуйте метод distance3d.
+//3. Используйте формулу
+//
+//
+//
+//4. Допишите тесты.
+//
+//
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 public class PointTest {
 
     @Test
-    public void distance() {
-        Assert.assertEquals(2.0, Point.distance(0, 0, 2, 0), 0.1);
+    public void distanceTwoPoint() {
+        Assert.assertEquals(2.0, new Point(0,0).distance(new Point(0,2)), 0.1);
+    }
+
+    @Test
+    public void distanceThreePoint() {
+        Assert.assertEquals(4.0, new Point(0,0,2).distance3d(new Point(0,0,6)), 0.1);
     }
 }
