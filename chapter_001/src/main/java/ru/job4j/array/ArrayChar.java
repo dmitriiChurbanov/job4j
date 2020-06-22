8c217ce71b024555a12a796eb812d3a2d754a216
Dmitrii Churbanov <dmitrii.churbanov@netcracker.com>
2020-06-22 12:19:39 +0400

    6.4. Слово начинается с ... [#228476] Добавил оператор брейк

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,6 +6,7 @@ public class ArrayChar {
         for (int i = 0, j = 0; i < pref.length; i++, j++) {
             if (pref[i] != word[j]) {
                 result = false;
+                break;
             }
         }
         return result;
