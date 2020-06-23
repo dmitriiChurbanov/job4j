bec01e483156034c07d1ad0067aa884348c7cce7
Dmitrii Churbanov <dmitrii.churbanov@netcracker.com>
2020-06-23 15:25:32 +0400

    6. Метод замены заявки. Tracker.replace [#228661]

--- chapter_002/src/main/java/tracker/Tracker.java
+++ chapter_002/src/main/java/tracker/Tracker.java
@@ -15,56 +15,43 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] item = new Item[this.size];
+        int count = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (items[i].getName().equals(key)) {
+                item[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(item, size);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < this.size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
     }
 
-    public Item[] findAll() {
-        Item[] item = new Item[items.length];
-        int count = 0;
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null) {
-                item[count] = items[i];
-                count++;
-            }
+    public boolean replace(int id, Item item) {
+        if (indexOf(id) != -1) {
+            item.setId(id);
+            items[indexOf(id)] = item;
+            return true;
+        } else {
+            return false;
         }
-        return Arrays.copyOf(item, count);
-    }
-
-    public Item[] findByName(String key) {
-        Item[] item = new Item[items.length];
-        int size = 0;
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].getName().equals(key)) {
-                item[size] = items[i];
-                size++;
-            }
-        }
-        item = Arrays.copyOf(item, size);
-        return item;
     }
 }
-//Метод public Item[] findAll() возвращает копию массива items без null элементов (без пустых ячеек).
-//
-//Перебирать все 100 элементов не нужно. У нас есть поле size.
-//Это поле и есть размер нового массива.
-//
-//Чтобы получить новый массив, нужно использовать метод Arrays.copyOf. Этот метод принимает два параметра: массив
-// элементов и новый размер.
-//Задание
-//
-//1. Добавьте недостающие методы в класс Tracker. В классе Tracker должны быть методы:
-//добавление заявок - public Item add(Item item);
-//получение списка всех заявок - public Item[] findAll();
-//получение списка по имени - public Item[] findByName(String key);
-//получение заявки по id - public Item findById(int id);
-//2. Создайте класс ru.job4j.tracker.StartUI. В нем создайте метод main.
-//    Внутри метода main создайте объект Tracker. Вызовите у него метод add с объектом Item.
-//    Вызовите метод findById и найденный Item выведите на консоль.
